e574311d6ac063d08d84553f2ee2c4afd98e0544
Bbrill <113665144+BillyYudha@users.noreply.github.com>
2025-08-08 18:37:24 +0700

    Delete JavaScript/belajarJS directory

--- JavaScript/belajarJS/waktu.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var dt = require('./myfirstmodule');
-
-http.createServer(function(req, res){
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write("Waktu dan tanggal kalian adalah : " + dt.myDateTime());
-    res.end();
-}).listen(8080);
